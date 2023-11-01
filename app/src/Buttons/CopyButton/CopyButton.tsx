3f27042d43b50632dc42ade0f75194eea1195f44
smh1988-2 <seamhurley1988@gmail.com>
2023-11-01 15:48:02 -0700

    copying logic

--- app/src/Buttons/CopyButton/CopyButton.tsx
+++ app/src/Buttons/CopyButton/CopyButton.tsx
@@ -1,18 +1,25 @@
-"use client"
+"use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./CopyButton.module.css";
 
 function CopyButton() {
+  const [copySuccess, setCopySuccess] = useState(false);
 
-    function handleCopyClick(e) {
-        console.log(e.target.parentElement.children[0].innerText)
-        console.log(e.target.parentElement.children[1].innerText)
-    }
+  function handleCopyClick(e: any) {
+    const shortUrl = e.target.parentElement.children[1].innerText;
+    navigator.clipboard.writeText(shortUrl);
+    setCopySuccess(true);
+  }
+
   return (
-    <button type="button" className={styles.copyBtn} onClick={e => handleCopyClick(e)}>
-      Copy
+    <button
+      type="button"
+      className={`${styles.copyBtn} ${styles.other}`}
+      onClick={(e) => handleCopyClick(e)}
+    >
+      {copySuccess ? "Copied" : "Copy"}
     </button>
   );
 }
