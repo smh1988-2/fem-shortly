70b674eac36a15a0e63d1646c964101825525f29
smh1988-2 <seamhurley1988@gmail.com>
2023-11-01 13:05:52 -0700

    styling for shortener

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import ConversionPanel from "./src/ConversionPanel/ConversionPanel";
 import Footer from "./src/Footer/Footer";
 import Hero from "./src/Hero/Hero";
 import NavBar from "./src/NavBar/NavBar";
+import Shortener from "./src/Shortener/Shortener";
 
 export default function Home() {
   return (
     <main>
       <NavBar />
       <Hero />
+      <Shortener />
       <CardDeck />
       <ConversionPanel content={"Boost your links today"} />
       <Footer />
--- app/src/Buttons/Button.tsx
+++ app/src/Buttons/Button.tsx
@@ -2,12 +2,13 @@ import styles from "./Button.module.css";
 
 export function Button({ ...props }) {
   const size = props.size;
+  const border = props.border;
   const type = props.type;
 
   return (
     <button
-      type="button"
-      className={`${styles.btn} ${styles[size]} ${styles[type]}`}
+      type={type}
+      className={`${styles.btn} ${styles[size]} ${styles[border]}`}
     >
       {props.content}
     </button>
